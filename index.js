b69c909d40baa6b83a32fb0df9bda03819cfbb5a
LittleCatBear <j.huguet9@gmail.com>
2014-12-25 20:33:04 +1300

    adding express to server

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 //1
-var http = require('http');
+var http = require('http'),
+	express = require('express');
+var app =  express();
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function (req, res){
+	res.send('<html><body><h1> Salut express</h1></body></html>');
+});
 
 //2
-http.createServer(function(req, res){
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end('<html><body><h1>Salut</h1></body></html>');
-
-}).listen(3000);
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Express server listening on port '+app.get('port'));
+});
 
 console.log('server running on port 3000');
