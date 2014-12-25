8eea828b5170ecb8f3f8481eecabe9152daa620a
LittleCatBear <j.huguet9@gmail.com>
2014-12-25 20:55:10 +1300

    routing example

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get('/', function (req, res){
 	res.send('<html><body><h1> Salut express</h1></body></html>');
 });
 
+app.get('/:a?/:b?/:c?', function(req, res){
+	res.send(req.params.a+' '+req.params.b+' '+req.params.c);
+});
+
 //2
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port '+app.get('port'));
