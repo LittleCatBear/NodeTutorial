1989027446953584541bde5998e0ffe0376ba8ef
LittleCatBear <j.huguet9@gmail.com>
2014-12-25 22:18:28 +1300

    debug path&404.jade

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 //1
 var http = require('http'),
-	express = require('express'),
-	path = require('path');
+	express = require('express');
+	//path = require('path');
 
 var app =  express();
+
 app.set('port', process.env.PORT || 3000);
-app.use(express.static(path.join(__dirname, 'public')));
+//app.set('views', path.join(__dirname, 'views"'));
+
+app.set('view engine', 'jade');
+
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res){
 	res.send('<html><body><h1> Salut express</h1></body></html>');
 });
 
-app.get('/:a?/:b?/:c?', function(req, res){
-	res.send(req.params.a+' '+req.params.b+' '+req.params.c);
+
+
+app.use(function(req, res){
+	res.render('404', {url:req.url});
 });
 
 //2
 http.createServer(app).listen(app.get('port'), function(){
-	console.log('Express server listening on port '+app.get('port'));
+	console.log('Express server listening on port ' + app.get('port'));
 });
-
-console.log('server running on port 3000');
