3da2bb91cf04f5d71eb827c0181f123c27899c9e
LittleCatBear <j.huguet9@gmail.com>
2014-12-25 20:16:08 +1300

    setup node

---
+++ index.js
@@ -0,0 +1,11 @@
+//1
+var http = require('http');
+
+//2
+http.createServer(function(req, res){
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.end('<html><body><h1>Salut</h1></body></html>');
+
+}).listen(3000);
+
+console.log('server running on port 3000');
